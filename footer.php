7a71527ef2fa77f00a47cfa8eb4ea1599bc8c80b
Ogoudikpe <98558214+Ogoudikpe@users.noreply.github.com>
2022-07-03 19:23:04 +0200

    taftaf
    projet

---
+++ footer.php
@@ -0,0 +1,157 @@
+
+<div class="modal fade" id="newtask" tabindex="-1" aria-labelledby="newtaskLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header border-0">
+                    <h5 class="modal-title" id="newtaskLabel">
+                        Nouvelle tâche à faire
+                    </h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <form>
+                    <div class="modal-body">
+                        <div class="mb-3">
+                            <textarea class="form-control mb-3" placeholder="Description" rows="5"></textarea>
+                        </div>
+                        <div>
+                            <div class="col-12">
+                                <input type="datetime-local" class="form-control">
+                            </div>
+                        </div>
+                    </div>
+                    <div id="submiter" class="modal-footer border-0 justify-content-center">
+                        <button type="submit" class="btn btn-dark">
+                            Enregistrer
+                        </button>
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                            Annuler
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="modal fade" id="edittask" tabindex="-1" aria-labelledby="edittaskLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header border-0">
+                    <h5 class="modal-title" id="edittaskLabel">
+                        Modifier tâche à faire
+                    </h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <form>
+                    <div class="modal-body">
+                        <div class="mb-3">
+                            <textarea class="form-control mb-3" placeholder="Description"
+                                rows="5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, hic eaque. Minima sint modi, harum ipsa aspernatur labore autem odit illum fuga esse facilis saepe incidunt, fugiat ea repudiandae ratione?</textarea>
+                        </div>
+                        <div>
+                            <div class="col-12">
+                                <input type="datetime-local" class="form-control" value="2022-04-29T15:19">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer border-0 justify-content-center">
+                        <button type="submit" class="btn btn-dark">
+                            Enregistrer
+                        </button>
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                            Annuler
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="modal fade" id="asign" tabindex="-1" aria-labelledby="asignLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header border-0">
+                    <h5 class="modal-title" id="asignLabel">
+                        Ajouter un collaborateur
+                    </h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="row container text-align">
+                        <div class="col-md-3 bg-light rounded-3 m-2">
+                            <a>
+                                <div class="d-flex justify-content-end">
+                                    <span class="rounded-pill badge bg-dark">
+                                        33
+                                    </span>
+                                </div>
+                                <div>
+                                    <img class="bg-dark rounded-circle" width="60px" height="60px">
+                                    <p>
+                                        John Doe
+                                    </p>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-md-3 bg-light rounded-3 m-2">
+                            <a> 
+                                <div class="d-flex justify-content-end">
+                                    <span class="rounded-pill badge bg-dark">
+                                        33
+                                    </span>
+                                </div>
+                                <div>
+                                    <img class="bg-dark rounded-circle" width="60px" height="60px">
+                                    <p>
+                                        John Doe
+                                    </p>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-md-3 bg-light rounded-3 m-2">
+                            <a>
+                                <div class="d-flex justify-content-end">
+                                    <span class="rounded-pill badge bg-dark">
+                                        33
+                                    </span>
+                                </div>
+                                <div>
+                                    <img class="bg-dark rounded-circle" width="60px" height="60px">
+                                    <p>
+                                        John Doe
+                                    </p>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-md-3 bg-light rounded-3 m-2">
+                            <a>
+                                <div class="d-flex justify-content-end">
+                                    <span class="rounded-pill badge bg-dark">
+                                        33
+                                    </span>
+                                </div>
+                                <div>
+                                    <img class="bg-dark rounded-circle" width="60px" height="60px">
+                                    <p>
+                                        John Doe
+                                    </p>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-md-3 bg-light rounded-3 m-2">
+                            <a>
+                                <div class="d-flex justify-content-end">
+                                    <span class="rounded-pill badge bg-dark">
+                                        33
+                                    </span>
+                                </div>
+                                <div>
+                                    <img class="bg-dark rounded-circle" width="60px" height="60px">
+                                    <p>
+                                        John Doe
+                                    </p>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
