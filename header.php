dc1cf29887660c6e263ef77d8926275f3f374843
Ogoudikpe <98558214+Ogoudikpe@users.noreply.github.com>
2022-07-03 19:27:33 +0200

    taftaf
    projet

---
+++ header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/bootstrap.css">
+    <link rel="stylesheet" href="./css/main.css">
+    <script src="./js/bootstrap.js"></script>
+    <title>Document</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="./logo/logo.png" alt="Logo de Taf-Taf" width="30" height="24"
+                    class="d-inline-block align-text-top">
+                TAF-TAF
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Toutes les tâches</a>
+                    </li>
+                    <li class="nav-item">
+                        <a type="button" class="nav-link" data-bs-toggle="modal" data-bs-target="#newtask">+ Nouvelle
+                            tâche</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">John Doe</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Quitter</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</body>
+</html>
