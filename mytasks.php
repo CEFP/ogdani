1a93dc7e1108f701f5e90cf8831b8faa5a675f34
Ogoudikpe <98558214+Ogoudikpe@users.noreply.github.com>
2022-07-03 19:33:36 +0200

    taftaf
    projet

---
+++ mytasks.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/bootstrap.css">
+    <script src="./js/bootstrap.js"></script>
+</head>
+
+<body>
+
+<?php include("header.php") ; ?>
+
+    <main class="mt-5 pt-3">
+        <div class="container-fluid">
+            <div class="d-flex d-block d-sm-flex justify-content-between">
+                <div>Mes taches / Liste des taches / Tache de John </div>
+                <div class="d-flex align-items-center justify-content-around">
+                    <span>Filtrage</span>
+                    <div class="m-1">
+                        <select class="form-control">
+                            <option value="">année</option>
+                            <option value="">2022</option>
+                            <option value="">2021</option>
+                        </select>
+                    </div>
+                    <div class="m-1">
+                        <select class="form-control">
+                            <option value="">mois</option>
+                            <option value="">Jan</option>
+                            <option value="">Fev<div style="background: silver;" class="rounded-2 p-2 mb-2">
+                            <p>invités les amis</p>
+                            <small style="font-size: 12px ;"> date limite: <br> 2022-05-01 22:30</small>
+                        </div></option>
+                            <option value="">Mar</option>
+                            <option value="">Avr</option>
+                            <option value="">Mai</option>
+                            <option value="">Juin</option>
+                            <option value="">Juillet</option>
+                            <option value="">Aout</option>
+                            <option value="">sept</option>
+                            <option value="">oct</option>
+                            <option value="">Nov</option>
+                            <option value="">Déc</option>
+                        </select>
+                    </div>
+                    <div class="m-1">
+                        <select class="form-control">
+                            <option value="">Jours</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6">6</option>
+                            <option value="7">7</option>
+                            <option value="8">8</option>
+                            <option value="9">9</option>
+                            <option value="10">10</option>
+                            <option value="11">11</option>
+                            <option value="12">12</option>
+                            <option value="13">13</option>
+                            <option value="14">14</option>
+                            <option value="15">15</option>
+                            <option value="16">16</option>
+                            <option value="17">17</option>
+                            <option value="18">18</option>
+                            <option value="19">19</option>
+                            <option value="20">20</option>
+                            <option value="21">21</option>
+                            <option value="22">22</option>
+                            <option value="23">23</option>
+                            <option value="24">24</option>
+                            <option value="25">25</option>
+                            <option value="26">26</option>
+                            <option value="27">27</option>
+                            <option value="28">28</option>
+                            <option value="29">29</option>
+                            <option value="30">30</option>
+                            <option value="31">31</option>
+                        </select>
+                    </div>
+                </div>
+
+            </div>
+            <div class="row">
+                <div class="col">
+                    <div class="text-center bg-primary rounded-6">en attente</div>
+                    <div class="bg-light p-2 vh-100">
+                        <div style="background: silver;" class="rounded-2 p-2 mb-2">
+                            <p>gdddtft fdtfgfu fcfgwdfgfrti esyresqw wyyresz sdytswxfuy ughuygiogfuyi oçyuyugg yyy-dggggkigftfidt </p>
+                            <small style="font-size: 12px ;"> date limite: 2022-05-01 22:30:00</small>
+                        </div>
+                        <div style="background: silver;" class="rounded-2 p-2 mb-2">
+                            <p>invités les amis</p>
+                            <small style="font-size: 12px ;"> date limite: 2022-05-01 22:30:00</small>
+                        </div>
+                        <div style="background: silver;" class="rounded-2 p-2 mb-2">
+                            <p>invités les amis</p>
+                            <small style="font-size: 12px ;"> date limite: 2022-05-01 22:30:00</small>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="text-center">en cours</div>
+                    <div class="bg-light p-2 vh-100">
+                        <div style="background: silver;" class="rounded-2 p-2 mb-2">
+                            <p>invités les amis</p>
+                            <small style="font-size: 12px ;"> date limite: 2022-05-01 22:30:00</small>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="text-center"> terminées</div>
+                    <div class="bg-light p-2 vh-100">
+                        <div style="background: silver;" class="rounded-2 p-2 mb-2">
+                            <p>invités les amis</p>
+                            <small style="font-size: 12px ;"> date limite: 2022-05-01 22:30:00</small>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="text-center">reportées</div>
+                    <div class="bg-light p-2 vh-100">
+                        <div style="background: silver;" class="rounded-2 p-2 mb-2">
+                            <p>invités les amis</p>
+                            <small style="font-size: 12px ;"> date limite: 2022-05-01 22:30:00</small>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="text-center">annulées</div>
+                    <div class="bg-light p-2 vh-100">
+                        <div style="background: silver;" class="rounded-2 p-2 mb-2">
+                            <p>invités les amis uvybbkgb gvvvvvvvvvkugytdx fcyfffffffffffffggg dyyyyyyyyyccgggg
+                                sdwqdqwrereeeee</p>
+                            <small style="font-size: 12px ;"> date limite: 2022-05-01 22:30:00</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <?php include("footer.php") ; ?>
+
+</body>
+
+</html>
