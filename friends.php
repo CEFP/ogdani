6185ca0b6cc22fcb56427cdb0e04adaf951c14b8
Ogoudikpe <98558214+Ogoudikpe@users.noreply.github.com>
2022-07-03 19:25:38 +0200

    taftaf
    proet

---
+++ friends.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/bootstrap.css">
+    <link rel="stylesheet" href="./css/main.css">
+    <script src="./js/bootstrap.js"></script>
+</head>
+
+<body>
+
+<?php include("header.php") ; ?>
+
+    <main class="mt-5 pt-3">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 mb-3  ">
+                    <a  href="" class="text-decoration-none">
+                        <div class="rounded-3 bg-light text-center p-3">
+                            <div class="float-end ">
+                                <span class="rounded-pill badge text-black">
+                                    33
+                                </span>
+                            </div>
+                            <div><img>
+                                <img  src="" alt="" class="bg-dark rounded-circle" width="64px" height="64px">
+                            </div>
+                            <div class="text-black">John Doe</div>
+                            <div class="text-black">joi@ghh.com</div>
+                            <div class="text-black">+4552455232</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-3 mb-3   ">
+                    <a>
+                        <div class="rounded-3 bg-light text-center p-3">
+                            <div class="float-end ">
+                                <span class="rounded-pill badge text-black">
+                                    33
+                                </span>
+                            </div>
+                            <div>
+                                <img class="bg-dark rounded-circle" width="64px" height="64px">
+                            </div>
+                            <div class="text-black">John Doe</div>
+                            <div class="text-black">joi@ghh.com</div>
+                            <div class="text-black">+4552455232</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-3 mb-3  ">
+                    <a>
+                        <div class="rounded-3 bg-light text-center p-3">
+                            <div class="float-end ">
+                                <span class="rounded-pill badge text-black">
+                                    33
+                                </span>
+                            </div>
+                            <div>
+                                <img class="bg-dark rounded-circle" width="64px" height="64px">
+                            </div>
+                            <div class="text-black">John Doe</div>
+                            <div class="text-black">joi@ghh.com</div>
+                            <div class="text-black">+4552455232</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-3 mb-3">
+                    <a>
+                        <div class="rounded-3 bg-light text-center p-3">
+                            <div class="float-end ">
+                                <span class="rounded-pill badge text-black">
+                                    33
+                                </span>
+                            </div>
+                            <div>
+                                <img class="bg-dark rounded-circle" width="64px" height="64px">
+                            </div>
+                            <div class="text-black">John Doe</div>
+                            <div class="text-black">joi@ghh.com</div>
+                            <div class="text-black">+4552455232</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-3 mb-3  ">
+                    <a>
+                        <div class="rounded-3 bg-light text-center p-3">
+                            <div class="float-end ">
+                                <span class="rounded-pill badge text-black">
+                                    33
+                                </span>
+                            </div>
+                            <div>
+                                <img class="bg-dark rounded-circle" width="64px" height="64px">
+                            </div>
+                            <div class="text-black">John Doe</div>
+                            <div class="text-black">joi@ghh.com</div>
+                            <div class="text-black">+4552455232</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-3 mb-3  ">
+                    <a>
+                        <div class="rounded-3 bg-light text-center p-3">
+                            <div class="float-end ">
+                                <span class="rounded-pill badge text-black">
+                                    33
+                                </span>
+                            </div>
+                            <div>
+                                <img class="bg-dark rounded-circle" width="64px" height="64px">
+                            </div>
+                            <div class="text-black">John Doe</div>
+                            <div class="text-black">joi@ghh.com</div>
+                            <div class="text-black">+4552455232</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-3 mb-3  ">
+                    <a>
+                        <div class="rounded-3 bg-light text-center p-3">
+                            <div class="float-end ">
+                                <span class="rounded-pill badge text-black">
+                                    33
+                                </span>
+                            </div>
+                            <div>
+                                <img class="bg-dark rounded-circle" width="64px" height="64px">
+                            </div>
+                            <div class="text-black">John Doe</div>
+                            <div class="text-black">joi@ghh.com</div>
+                            <div class="text-black">+4552455232</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-3 mb-3  ">
+                    <a>
+                        <div class="rounded-3 bg-light text-center p-3">
+                            <div class="float-end ">
+                                <span class="rounded-pill badge text-black">
+                                    33
+                                </span>
+                            </div>
+                            <div>
+                                <img class="bg-dark rounded-circle" width="64px" height="64px">
+                            </div>
+                            <div class="text-black">John Doe</div>
+                            <div class="text-black">joi@ghh.com</div>
+                            <div class="text-black">+4552455232</div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+
+        </div>
+    </main>
+
+    <?php include("footer.php") ; ?>
+
+</body>
+
+</html>
